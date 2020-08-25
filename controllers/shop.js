3e18567ac43411d537993c28cae0ce4e7c4eb3ba
Attila <59293104+sweeptic@users.noreply.github.com>
2020-08-25 18:52:42 +0200

    add getindex page

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,55 +1,66 @@
+const Product = require("../model/product");
+
+
 exports.getIndex = (req, res, next) => {
-   res.render('/xx-xx', {
-     pageTitle: 'xx xx',
-     path: '/xxx'
-   });
- };
+  const cb =(product) => (
+    res.render('shop/index', {
+      prods: product,
+      pageTitle: 'Shop',
+      path: '/'
+    })
+  )
+  Product.fetchAll(cb);
+};
 
- exports.getProducts = (req, res, next) => {
-   res.render('/xx-xx', {
-     pageTitle: 'xx xx',
-     path: '/xxx'
-   });
- };
+exports.getProducts = (req, res, next) => {
+  const cb =(product) => (
+    res.render('shop/product-detail', {
+      prods: product,
+      pageTitle: 'Shop',
+      path: '/products'
+    })
+  )
+  Product.fetchAll(cb);
+};
 
- exports.getProduct = (req, res, next) => {
-   res.render('/xx-xx', {
-     pageTitle: 'xx xx',
-     path: '/xxx'
-   });
- };
+exports.getProduct = (req, res, next) => {
+  res.render('/xx-xx', {
+    pageTitle: 'xx xx',
+    path: '/xxx'
+  });
+};
 
- exports.getCart = (req, res, next) => {
-   res.render('/xx-xx', {
-     pageTitle: 'xx xx',
-     path: '/xxx'
-   });
- };
+exports.getCart = (req, res, next) => {
+  res.render('/xx-xx', {
+    pageTitle: 'xx xx',
+    path: '/xxx'
+  });
+};
 
- exports.postCart = (req, res, next) => {
-   res.render('/xx-xx', {
-     pageTitle: 'xx xx',
-     path: '/xxx'
-   });
- };
+exports.postCart = (req, res, next) => {
+  res.render('/xx-xx', {
+    pageTitle: 'xx xx',
+    path: '/xxx'
+  });
+};
 
- exports.postCartDeleteProduct = (req, res, next) => {
-   res.render('/xx-xx', {
-     pageTitle: 'xx xx',
-     path: '/xxx'
-   });
- };
+exports.postCartDeleteProduct = (req, res, next) => {
+  res.render('/xx-xx', {
+    pageTitle: 'xx xx',
+    path: '/xxx'
+  });
+};
 
- exports.getOrders = (req, res, next) => {
-   res.render('/xx-xx', {
-     pageTitle: 'xx xx',
-     path: '/xxx'
-   });
- };
+exports.getOrders = (req, res, next) => {
+  res.render('/xx-xx', {
+    pageTitle: 'xx xx',
+    path: '/xxx'
+  });
+};
 
- exports.getCheckout = (req, res, next) => {
-   res.render('/xx-xx', {
-     pageTitle: 'xx xx',
-     path: '/xxx'
-   });
- };
+exports.getCheckout = (req, res, next) => {
+  res.render('/xx-xx', {
+    pageTitle: 'xx xx',
+    path: '/xxx'
+  });
+};
