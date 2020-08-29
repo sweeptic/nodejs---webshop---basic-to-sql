c3aa981bf3cc148fdf91c35ebc118973ddb5fee6
Attila <59293104+sweeptic@users.noreply.github.com>
2020-08-29 16:27:46 +0200

    delete items from cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -24,55 +24,60 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getProduct = (req, res, next) => {
-
   const prodId = req.params.productId;
-
   const cb = (product) => (
     res.render('shop/product-detail', {
       product: product,
       pageTitle: product.title,
       path: '/products'
     }));
-
   Product.findById(prodId, cb);
 };
 
+
 exports.getCart = (req, res, next) => {
 
+  const cb = cart => {
 
-  res.render('shop/cart', {
-    pageTitle: 'xx xx',
-    path: '/cart',
-    pageTitle: 'Your Cart',
-    products: null
-  });
+    const cb2 = products => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(prod => prod.id === product.id);
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts
+      });
+    }
 
+    Product.fetchAll(cb2)
+  }
 
+  Cart.getCart(cb)
 };
 
 exports.postCart = (req, res, next) => {
-
   const prodId = req.body.productId;
-
   //definialunk egy cb-t. itt meg nem hivjuk meg.
   const cb = (product) => {
     Cart.addProduct(prodId, product.price)
   }
-
   //prodid + cb atadjuk ennek a findByIdnak es meghivjuk
   Product.findById(prodId, cb);
-
   res.redirect('/cart');
-
-
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
-  res.render('/xx-xx', {
-    pageTitle: 'xx xx',
-    path: '/xxx'
-  });
-};
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  })
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
